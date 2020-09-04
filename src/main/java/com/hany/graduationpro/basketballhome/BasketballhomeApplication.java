2868088661846cd9927ddd03a4463cea7dbac260
hanyu <1204674020@qq.com>
2020-09-04 23:24:10 +0800

    提交测试

---
+++ src/main/java/com/hany/graduationpro/basketballhome/BasketballhomeApplication.java
@@ -0,0 +1,13 @@
+package com.hany.graduationpro.basketballhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketballhomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasketballhomeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hany/graduationpro/basketballhome/BasketballhomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hany.graduationpro.basketballhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasketballhomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
